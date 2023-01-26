4ecee0146a58c2bc7bc0ace7bf3927c36fa0afe9
I'm Jaystar <58359895+Jaystar-Bee@users.noreply.github.com>
2023-01-26 22:45:56 +0100

    added extra design

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -9,15 +9,16 @@ const Modal = (props) => {
     ></div>
   );
   const modal = (
-    <div className="fixed z-50 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-dark-light rounded-lg min-w-[24rem] sm:w-[32rem] md:w-[52rem] min-h-[22rem] p-8 text-white">
-      <h2 className="font-bold text-2xl mb-2">{props.title}</h2>
+    <div className="fixed z-50 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-dark-light rounded-lg min-w-[18rem] xs:min-w-[28rem] sm:w-[36rem] md:w-[52rem] min-h-[16rem] p-8 text-white">
+      <h2 className="font-bold text-lg sm:text-2xl mb-2">{props.title}</h2>
       <iframe
         width="100%"
-        className="h-[24rem]"
-        title="123"
+        className="h-[12rem] sm:h-[24rem]"
+        title={props.title}
         src={
-          props.link || "https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"
+          props.link || "https://www.youtube.com/embed/ma7rUS_vW9M?autoplay=1"
         }
+        allow="web-share; autoplay"
         frameborder="0"
       ></iframe>
       <div className="flex justify-end mt-6">
